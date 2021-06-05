3d9ccdf603b6e53f33768a9db70567267f250991
bboing4444 <85376895+bboing4444@users.noreply.github.com>
2021-06-05 10:10:16 +0900

    1st
    it works

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Converter_dotNet
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Run_Decoder.Click += Button_Click;
+            this.Run_Encoder.Click += Button_Click;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == this.Run_Decoder)
+            {
+                string input = this.Decoder_Input.Text;
+                string output = Base64Decode(input);
+
+                if (output == null)
+                    this.Decoder_Output.Text = "Conversion Error";
+                else
+                    this.Decoder_Output.Text = output;
+            }
+            else if (sender == this.Run_Encoder)
+            {
+                string input = this.Encoder_Input.Text;
+                string output = Base64Encode(input);
+
+                if (output == null)
+                    this.Encoder_Output.Text = "Conversion Error";
+                else
+                    this.Encoder_Output.Text = output;
+            }
+        }
+
+        public static string Base64Encode(string data)
+        {
+            try
+            {
+                byte[] encData_byte = new byte[data.Length];
+                encData_byte = System.Text.Encoding.UTF8.GetBytes(data);
+                string encodedData = Convert.ToBase64String(encData_byte);
+                return encodedData;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+
+        public static string Base64Decode(string data)
+        {
+            try
+            {
+                System.Text.UTF8Encoding encoder = new System.Text.UTF8Encoding();
+                System.Text.Decoder utf8Decode = encoder.GetDecoder();
+
+                byte[] todecode_byte = Convert.FromBase64String(data);
+                int charCount = utf8Decode.GetCharCount(todecode_byte, 0, todecode_byte.Length);
+                char[] decoded_char = new char[charCount];
+                utf8Decode.GetChars(todecode_byte, 0, todecode_byte.Length, decoded_char, 0);
+                string result = new String(decoded_char);
+                return result;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+    }
+}
